188ef6c78200d3108d6c1c139b319f09bc670cf8
Kazhymurat Assanali <kigirin462600@gmail.com>
2021-03-04 23:42:26 +0600

    CARconte

--- src/com/company/controllers/DriverController.java
+++ src/com/company/controllers/DriverController.java
@@ -2,46 +2,46 @@ package com.company.controllers;
 
 import com.company.entities.Car;
 import com.company.entities.Driver;
-
+import com.company.repositories.CarRepo;
+import com.company.repositories.interfaces.ICarRepository;
+import com.company.repositories.interfaces.IDriverRepository;
 import java.util.List;
-
 public class DriverController {
-    private final ICarRepository repo;
-    private final IDriverRepository driverRepo;
-    public DriverController(IDriverRepository repo , ICarRepository driverRepo){
-        this.repo = repo;
-        this.driverRepo = driverRepo;
+    private final ICarRepository carRepository;
+    private final IDriverRepository repository;
+    public DriverController(IDriverRepository repository , ICarRepository carRepository){
+        this.carRepository = carRepository;
+        this.repository = repository;
     }
-    public String createDriver(String name , String surname , String phoneNumber , int CarID){
-        Driver driver = new Driver(name , surname , phoneNumber , CarID);
-
-        boolean created = repo.createDriver(driver);
-
-        return (created ? "Driver was created!" : "Driver creation was failed!");
+    /*    private int id;
+        private String name;
+        private String surname;
+        private String PhoneNumber;
+        private int CarID;*/
+    public String createDriver(String name , String surname , String PhoneNumber , int CarID){
+        Driver driver = new Driver(name , surname , PhoneNumber , CarID);
+        boolean created = repository.createDriver(driver);
+        return (created ? "Driver was created!" : "Driver creation was failed");
     }
     public String getDriver(int id){
-        Driver driver = repo.getDriver(id);
-        return (driver == null ? "Driver was not found!" : train.toString());
+        Driver driver = repository.getDriver(id);
+        return (driver == null ? "Driver was not found!" : driver.toString());
     }
-
     public String getAllDrivers(){
-        List<Driver> drivers = repo.getAllDrivers();
+        List<Driver> drivers = repository.getAllDrivers();
         return drivers.toString();
     }
-    public  String createCar(String name){
-        Car car = new Car(name);
-
-        boolean created= repo.createCar(Car);
+    public  String createCar(String model , String number , String colour){
+        Car car = new Car(model , number , colour);
+        boolean created= carRepository.createCar(car);
         return (created ? "Car was created!" : "Car creation was failed");
     }
-    public Car String getCar(int id){
-        Car car = CarRepo.getCar(id);
-        return car;
+    public String getCar(int id){
+        Car car = carRepository.getCar(id);
+        return (car == null ? "Car was not found" : car.toString());
     }
-
     public String getAllCars(){
-        List<Car> Cars = repo.getAllCars();
+        List<Car> Cars = carRepository.getAllCars();
         return Cars.toString();
     }
-
 }
