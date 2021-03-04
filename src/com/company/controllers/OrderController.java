0238b4be241497b438d6ed810ecab21312be229a
Kazhymurat Assanali <kigirin462600@gmail.com>
2021-03-04 23:43:43 +0600

    CARconte

--- src/com/company/controllers/OrderController.java
+++ src/com/company/controllers/OrderController.java
@@ -1,26 +1,30 @@
 package com.company.controllers;
 
+import com.company.entities.Order;
 import java.util.List;
-
 public class OrderController {
     private com.company.repositories.interfaces.IOrderRepository repo;
     public OrderController (com.company.repositories.interfaces.IOrderRepository repo)
     {
         this.repo=repo;
     }
-
-    public String createOrder( String  )
+    /*    private double cost ;
+        private String point_A;
+        private String point_B;
+        private String date;
+        private int driverID;*/
+    public String createOrder(double cost , String point_A , String point_B , String date , int driverID)
     {
-
+        Order order = new Order(cost , point_A , point_B , date , driverID);
+        boolean created = repo.createOrder(order);
+        return (created ? "Order was created!" : "Order creation was failed!");
     }
-
     public String getOrder(int id){
         com.company.entities.Order order=repo.getOrder(id);
-        return (order== null ? "Ordder was not found! " : order.toString());
+        return (order== null ? "Order was not found! " : order.toString());
     }
     public String getAllOrders(){
         List<Order> Orders = repo.getAllOrders();
         return Orders.toString();
     }
-
 }
