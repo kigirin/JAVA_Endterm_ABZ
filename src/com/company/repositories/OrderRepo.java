f860a1447c6967822fc4313be97bac09570c0fb9
recentnoise <zhanibekbalabaev@gmail.com>
2021-03-05 16:11:42 +0600

    Repository by Zhanibek BD-2005

--- src/com/company/repositories/OrderRepo.java
+++ src/com/company/repositories/OrderRepo.java
@@ -1,4 +1,110 @@
 package com.company.repositories;
 
-public class OrderRepo {
+import com.company.data.interfaces.IDB;
+import com.company.entities.Order;
+import com.company.repositories.interfaces.IOrderRepository;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderRepo implements IOrderRepository {
+    private final IDB db;
+    public OrderRepo(IDB db){
+        this.db = db;
+    }
+/*    private int id;
+    private double cost ;
+    private String point_A;
+    private String point_B;
+    private String date;
+    private int driverID;*/
+    @Override
+    public boolean createOrder(Order order){
+        Connection con = null;
+        try{
+            con = db.getConnection();
+            String sql = "INSERT INTO Orders(id , cost , point_A , point_B , date , driverID) VALUES (?,?,?,?,?,?)";
+            PreparedStatement st = con.prepareStatement(sql);
+
+            st.setInt(1,order.getId());
+            st.setDouble(2,order.getCost());
+            st.setString(3, order.getPoint_A());
+            st.setString(4,order.getPoint_B());
+            st.setString(5,order.getDate());
+            st.setInt(6,order.getDriverID());
+
+            boolean executed = st.execute();
+            return executed;
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException throwables){
+                throwables.printStackTrace();
+            }
+        }
+        return false;
+    }
+    @Override
+    public Order getOrder(int id){
+        Connection con = null;
+        try{
+            con = db.getConnection();
+            String sql = "SELECT id , cost , point_A , point_B , date , driverID FROM Drivers WHERE id=?";
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setInt(1,id);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                Order order = new Order(rs.getInt("id"), rs.getDouble("cost"),
+                        rs.getString("Point_A") , rs.getString("Point_B") ,
+                        rs.getString("date"), rs.getInt("driverID"));
+                return null;
+            }
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } finally {
+            try{
+                con.close();
+            } catch (SQLException throwables){
+                throwables.printStackTrace();
+            }
+        }
+        return null;
+    }
+    @Override
+    public List<Order> getAllOrders(){
+        Connection con = null;
+        try{
+            con = db.getConnection();
+            String sql = "SELECT id , cost , point_A , point_B , date , driverID FROM Orders";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            List<Order> orders = new ArrayList<>();
+            while (rs.next()){
+                Order order = new Order(rs.getInt("id"), rs.getDouble("cost"),
+                        rs.getString("Point_A") , rs.getString("Point_B") ,
+                        rs.getString("date"), rs.getInt("driverID"));
+                orders.add(order);
+            }
+            return orders;
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } finally {
+            try{
+                con.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return null;
+    }
 }
